f1f0f4b818b7628147976e74148791c2ab01498b
Esteban Silva <esilvac59@gmail.com>
2023-10-25 16:34:01 -0500

    Remove @floating-ui/react-dom library and all logic related to it

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  */
 
 // Core React
-import React, { useState, useEffect, useLayoutEffect, useReducer } from 'react'
+import React, { useState, useEffect, useCallback, useReducer } from 'react'
 import { createPortal } from 'react-dom'
 import styled from '@emotion/styled'
 import PropTypes from 'prop-types'
@@ -15,7 +15,6 @@ import classNames from 'classnames'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons/faAngleDoubleDown'
 import scrollIntoView from 'scroll-into-view'
-import { useFloating, shift, size, getScrollParents } from '@floating-ui/react-dom'
 
 // Utils
 import { useIdentifier, useAutocomplete, useKeyboardScroll } from '@joeyparis/hooks'
@@ -90,7 +89,7 @@ const InputContainer = styled.div`
 	// }
 
 	&.fl-input-container-msg-show {
-		margin-bottom: 1.5em;
+		padding-bottom: 1.5em;
 	}
 
 	input:not(:focus):not(.fl-valid):not(.fl-invalid) {
@@ -327,7 +326,7 @@ const InputContainer = styled.div`
 	}
 
 	.fl-msg {
-		bottom: -1.75em;
+		bottom: 0;
 		display: none;
 		font-size: 13px;
 		overflow: hidden;
@@ -396,7 +395,7 @@ const FloatingLabel = ({
 	const label_autofill_id = useIdentifier()
 
 	const [is_focused, setIsFocused] = useState(false)
-	const [size_data, setSizeData] = useState({})
+	const [dropdown_position, setDropdownPosition] = useState({})
 
 	const [visible, setVisible] = useReducer((state, action) => {
 		if (action.is_visible) {
@@ -412,25 +411,6 @@ const FloatingLabel = ({
 
 	const initial_options = options || all_autocomplete_options
 	const selected_option = initial_options && initial_options.find((o) => String(o.value) === String(value))
-
-	const {
-		x: dropdown_x,
-		y: dropdown_y,
-		reference: dropdown_reference,
-		floating: dropdown_floating,
-		strategy: dropdown_strategy,
-		refs,
-		update,
-	} = useFloating({
-		placement: 'bottom-start',
-		portaled: true,
-		middleware: [
-			size({
-				apply: setSizeData,
-			}),
-			shift(),
-		],
-	})
 
 	const { handleKeyboardScroll, active_index, visible_options, updateOptions } = useKeyboardScroll({
 		initial_options,
@@ -453,7 +433,7 @@ const FloatingLabel = ({
 	const is_autocomplete = !!autocomplete_strings
 	const is_select = input_props.type === 'select'
 	const is_list = input_props.type === 'list'
-	const display_value = is_select && selected_option ? selected_option.text : value || ''
+	const display_value = is_select && selected_option ? selected_option.text : value ?? ''
 
 	// Prevent autofill if requested or if input is select type or if autocomplete options provided
 	const should_prevent_autofill = prevent_autofill || is_select || is_autocomplete
@@ -489,30 +469,6 @@ const FloatingLabel = ({
 		}
 	}, [active_index])
 
-	useEffect(() => {
-		if (!refs.reference.current || !refs.floating.current) {
-			return () => null
-		}
-
-		const parents = [...getScrollParents(refs.reference.current), ...getScrollParents(refs.floating.current)]
-
-		parents.forEach((parent) => {
-			parent.addEventListener('scroll', update)
-			parent.addEventListener('resize', update)
-		})
-
-		return () => {
-			parents.forEach((parent) => {
-				parent.removeEventListener('scroll', update)
-				parent.removeEventListener('resize', update)
-			})
-		}
-	}, [refs.reference, refs.floating, update])
-
-	useLayoutEffect(() => {
-		update()
-	}, [is_focused])
-
 	if (is_select) {
 		input_props.type = 'text' // eslint-disable-line no-param-reassign
 	}
@@ -540,6 +496,18 @@ const FloatingLabel = ({
 		return false
 	}
 
+	const measuredRef = useCallback((node) => {
+		if (node !== null) {
+			const positions = node.getBoundingClientRect()
+
+			if (!positions) return {}
+
+			setDropdownPosition({ top: positions.top, left: positions.left, width: positions.width })
+		}
+
+		return true
+	}, [])
+
 	return (
 		<InputContainer
 			className={classNames('fl-input-container', containerClassNames, {
@@ -547,7 +515,6 @@ const FloatingLabel = ({
 				'fl-input-container-msg-show': has_message,
 			})}
 			style={containerStyle}
-			ref={dropdown_reference}
 		>
 			{is_select && (
 				<FontAwesomeIcon
@@ -624,7 +591,6 @@ const FloatingLabel = ({
 						})
 					}}
 					onKeyPress={(e) => {
-						console.info(e.key)
 						if (e.key === 'Enter') {
 							onChange({
 								target: {
@@ -644,18 +610,14 @@ const FloatingLabel = ({
 				!input_props.disabled &&
 				createPortal(
 					<DropdownSelect
+						title={`${label} Dropdown`}
 						style={{
-							...listStyle,
-							position: dropdown_strategy,
+							position: 'absolute',
 							top: '0',
 							left: '0',
-							transform: `translate(${Math.round(dropdown_x)}px,${Math.round(dropdown_y)}px)`,
-							maxHeight: size_data.height ? size_data.height - 50 : '',
-							width: refs.reference.current?.getBoundingClientRect().width,
+							...dropdown_position,
 							display: is_focused ? 'block' : 'none',
 						}}
-						title={`${label} Dropdown`}
-						ref={dropdown_floating}
 					>
 						{visible_options.map((option, index) => (
 							<Option
@@ -683,6 +645,7 @@ const FloatingLabel = ({
 					</DropdownSelect>,
 					document.body,
 				)}
+			<div ref={measuredRef} style={{ position: 'relative', order: 10 }} />
 		</InputContainer>
 	)
 }
@@ -729,3 +692,4 @@ FloatingLabel.defaultProps = {
 }
 
 export default FloatingLabel
+
